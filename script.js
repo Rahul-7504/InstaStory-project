7718600a4663ccee4873466b24665d3f8950e8a2
Rahul-Sonawane <146803765+Rahul-7504@users.noreply.github.com>
2023-12-11 06:12:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let arr = [
+    {
+        dp:"images/rahul1.jpg",
+        story:"images/rahul2.jpg"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+        story:"https://images.unsplash.com/photo-1517246375798-77649c539a8c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEyfHxtb2RlbHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fG1vZGVsfGVufDB8fDB8fHww",
+        story:"https://images.unsplash.com/photo-1604681630513-69474a4e253f?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1618641986557-1ecd230959aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym95c3xlbnwwfHwwfHx8MA%3D%3D"
+    }
+];
+
+let clutter = ""
+let storiyan = document.querySelector("#storiyan")
+
+arr.forEach(function(elem,idx){
+
+    clutter += ` <div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+</div>`
+});
+storiyan.innerHTML = clutter;
+
+storiyan.addEventListener("click",function(dets){
+    document.querySelector("#full-screen").style.display = "block"
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display = "none"
+    },2000);
+});
